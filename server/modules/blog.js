945d11913937bfab84facde680e4389fff29c5df
Serbay ACAR <serbayacar@gmail.com>
2018-06-16 12:28:20 +0300

    Classes are added

---
+++ server/modules/blog.js
@@ -0,0 +1,63 @@
+function Blog(id,user, email, title, content ) {
+  this.id = id;
+  this.author = {
+    'userID': userID,
+    'first': firstName,
+    'last' : lastName,
+    'fullName': firstName + ' ' + lastName,
+    'username': username,
+    'email': email,
+  };
+  this.title = title;
+  this.content = content;
+}
+
+Blog.prototype = {
+    constructor: Blog
+    , setAuthor: function (userID, firstName, lastName, username,email) {
+    	this.author = {
+                        'userID': userID,
+                        'first': firstName,
+                        'last' : lastName,
+                        'fullName': firstName + ' ' + lastName,
+                        'username': username,
+                        'email': email,
+                      };
+    }, setUserID: function (userID) {
+    	this.author['userID'] = userID;
+    }, setFirstName: function (firstName) {
+        this.author['first'] = firstName;
+    }, setLastName: function (lastName) {
+        this.author['last'] = lastName;
+    }, setFullName: function (firstName, lastName) {
+        this.author['first'] = firstName;
+        this.author['last'] = lastName;
+        this.author['fullName'] = firstName + ' ' + lastName;
+    }, setEmail: function (email) {
+    	this.author['email']  = email;
+    }, setTitle: function (title) {
+    	this.title = title;
+    }, setContent: function (content) {
+    	this.content = content;
+    }, getID: function() {
+        return this.id;
+    }, getAuthor: function() {
+        return this.author;
+    }, getUserID: function() {
+        return this.author['userID'];
+    }, getFirstName: function() {
+        return this.author['first'];
+    }, getLastName: function() {
+        return this.author['last'];
+    }, getFullName: function() {
+        return [this.author['first'],this.author['last']];
+    }, getUserName: function() {
+        return this.author['username'];
+    }, getEmail: function() {
+        return this.author['email'];
+    }, getTitle: function() {
+        return this.title;
+    }, getContent: function() {
+        return this.content;
+    }
+}
---
+++ server/modules/users.js
@@ -0,0 +1,50 @@
+function User(id,username, password, email, firstName, lastName) {
+  this.id = id;
+  this.name = {
+    'first': firstName,
+    'last' : lastName,
+  };
+  this.username = username;
+  this.email = email;
+  this.password = password;
+}
+
+User.prototype = {
+    constructor: User
+    , setFullName: function (firstName, lastName) {
+    	this.name = {
+					   'first': firstName,
+					   'last' : lastName,
+					};
+    }, setFirstName: function (firstName) {
+    	this.name['first'] = firstName;
+    }, setLastName: function (lastName) {
+    	this.name['last'] = lastName;
+    }, setEmail: function (email) {
+    	this.email = email;
+    }, setPassword: function (password) {
+    	this.password = password;
+    }, setGithubAddress: function (githubAddress) {
+    	this.social['github'] = githubAddress;
+    }, setLinkedInAddress: function (linkedInAddress) {
+    	this.social['linkedin'] = linkedInAddress;
+    }, getID: function() {
+        return this.id;
+    }, getFullName: function() {
+        return this.name;
+    }, getFirstName: function() {
+        return this.name['first'];
+    }, getLastName: function() {
+        return this.name['last'];
+    }, getUsername: function() {
+        return this.username;
+    }, getEmail: function() {
+        return this.email;
+    }, getPassword_hr: function() {
+        return this.password;
+    }, getPassword_md5: function() {
+        return this.password;
+    }, getSocialMedia: function() {
+        return this.social;
+    }
+}
