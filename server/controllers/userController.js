a7a3f5b1909f62f73caf84e0957ff5de0fa4e13f
Serbay ACAR <serbayacar@gmail.com>
2018-06-16 12:32:29 +0300

    userController v1.0

---
+++ server/controllers/userController.js
@@ -0,0 +1,11 @@
+//Still Developed
+
+
+var user = require('../modules/users.js');
+var db = firebase.database();
+var userRef= db.child('users');
+
+module.exports.addUser = function (req,res){
+
+	usersRef.set();
+}
