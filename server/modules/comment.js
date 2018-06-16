77276d764c86bf9b3eebb22237697dc9f41ea9e3
Serbay ACAR <serbayacar@gmail.com>
2018-06-16 21:44:29 +0300

    comment class added

--- server/modules/comment.js
+++ server/modules/comment.js
@@ -1,28 +1,24 @@
 //Still Developed
 
-function Comment(id,user, email, title, content ) {
+function Comment(id,user, blog, comment ) {
   this.id = id;
   this.author = {
-    'userID': userID,
     'first': firstName,
     'last' : lastName,
     'fullName': firstName + ' ' + lastName,
-    'username': username,
     'email': email,
   };
-  this.title = title;
-  this.content = content;
+  this.blog = blog;
+  this.comment = comment;
 }
 
 Comment.prototype = {
     constructor: Comment
-    , setAuthor: function (userID, firstName, lastName, username,email) {
+    , setAuthor: function (firstName, lastName,email) {
     	this.author = {
-                        'userID': userID,
                         'first': firstName,
                         'last' : lastName,
                         'fullName': firstName + ' ' + lastName,
-                        'username': username,
                         'email': email,
                       };
     }, setUserID: function (userID) {
@@ -36,30 +32,26 @@ Comment.prototype = {
         this.author['last'] = lastName;
         this.author['fullName'] = firstName + ' ' + lastName;
     }, setEmail: function (email) {
-    	this.author['email']  = email;
-    }, setTitle: function (title) {
-    	this.title = title;
-    }, setContent: function (content) {
-    	this.content = content;
+        this.author['email']  = email;
+    }, setBlog: function (blog) {
+        this.blog  = blog;
+    }, setComment: function (comment) {
+    	this.comment = comment;
     }, getID: function() {
         return this.id;
     }, getAuthor: function() {
         return this.author;
-    }, getUserID: function() {
-        return this.author['userID'];
     }, getFirstName: function() {
         return this.author['first'];
     }, getLastName: function() {
         return this.author['last'];
     }, getFullName: function() {
         return [this.author['first'],this.author['last']];
-    }, getUserName: function() {
-        return this.author['username'];
     }, getEmail: function() {
         return this.author['email'];
-    }, getTitle: function() {
-        return this.title;
-    }, getContent: function() {
-        return this.content;
+    }, getBlog: function() {
+        return this.blog;
+    }, getComment: function() {
+        return this.comment;
     }
 }
