994117c3ee7a787b3007da84f374615f59f7439f
Serbay ACAR <serbayacar@gmail.com>
2018-06-17 22:35:07 +0300

    clerifying for irregelant

--- server/routers/blogRouter.js
+++ server/routers/blogRouter.js
@@ -6,9 +6,7 @@ var router = express.Router();
 var controller = require('../controllers/blogController');
 
 //GET Functions
-router.get('/', controller.index);
 
 //POST Functions
-router.post('/contact', controller.sentEmail);
 
 module.exports = router;
--- server/routers/commentRouter.js
+++ server/routers/commentRouter.js
@@ -6,9 +6,9 @@ var router = express.Router();
 var controller = require('../controllers/commentController');
 
 //GET Functions
-router.get('/', controller.index);
+
 
 //POST Functions
-router.post('/contact', controller.sentEmail);
+
 
 module.exports = router;
