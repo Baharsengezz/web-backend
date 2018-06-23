652fb7f3badbd983eb23d0d6fca016102d86d95a
Furkan Taha ÖNDER <furkantahaonder@gmail.com>
2018-06-24 01:27:49 +0300

    Blog_Endpoint :: adding setAuthor  updateBlog
    Blog_Endpoint :: adding setAuthor  updateBlog deleteBlog and in the createBlog adding publish date

--- server/controllers/blogController.js
+++ server/controllers/blogController.js
@@ -8,6 +8,7 @@ let firebase = require("firebase-admin");
 module.exports.createBlog = function (req,res){
 
     let userRef = firebase.database().ref().child('user');
+    var publishDate = new Date();
 
     userRef.orderByChild('username').equalTo(req.query.uname).once("value").then( function(snapshot) {
 
@@ -17,11 +18,11 @@ module.exports.createBlog = function (req,res){
              // A new Blogger
             let blogUser = new User( data.key, data.val().username, data.val().password, data.val().email, data.val().name.first, data.val().name.last );
 
-            let newBlog = new Blog(bloggerKey, blogUser ,req.query.title,req.query.content)
+            let newBlog = new Blog(bloggerKey, blogUser ,req.query.title,req.query.content,publishDate)
 
           // Write the new blog simultaneously in the blog list.
           let commits = {};
-          commits['/blog/'+ data.val().username+'/' + bloggerKey] = newBlog;
+          commits['/blog/'+ data.val().username + '/' + bloggerKey] = newBlog;
           firebase.database().ref().update(commits);
           return true;
         });
@@ -30,4 +31,53 @@ module.exports.createBlog = function (req,res){
     return true;
 }
 
+module.exports.setAuthor = function (req,res){
 
+  let blogRef = firebase.database().ref().child('blogUser');
+
+  blogRef.orderByChild('username').equalTo(req.query.uname).once("value").then( function(snapshot) {
+
+      snapshot.forEach(function(data) {
+
+
+      });
+
+  });
+  return true;
+}
+
+module.exports.updateBlog = function (req,res){
+
+  let blogRef = firebase.database().ref().child('blogUser');
+
+  blogRef.orderByChild('username').equalTo(req.query.uname).once("value").then( function(snapshot) {
+
+      snapshot.forEach(function(data) {
+         // A new blogUser entry.
+        let updatingBlogUser = new User( data.key, data.val().username, data.val().password, data.val().email, data.val().name.first, data.val().name.last );
+
+        // Updating blogUser
+        updatingBlogUser.setAuthor(  );
+
+        // Update the blogUser data simultaneously in the existing blogUser list.
+        blogRef.child(data.key).update(updatingBlogUser);
+      });
+
+  });
+  return true;
+}
+
+module.exports.deleteBlog = function (req,res){
+
+  let blogRef = firebase.database().ref().child('blogUser');
+
+  blogRef.orderByChild('username').equalTo(req.query.uname).once("value").then( function(snapshot) {
+
+      snapshot.forEach(function(data) {
+        // Delete the blog data simultaneously in the existing blog list.
+        blogRef.child(data.key).remove();
+      });
+
+  });
+  return true;
+}
